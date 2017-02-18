44c55fa70589f99380da229bc3bc4b725658dd73
VitoZet <slava@gigel.ru>
2017-02-18 13:05:33 +0300

    list ok next save in csv

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import xlrd
 
-wb = xlrd.open_workbook('TestMino.xlsx')
+wb = xlrd.open_workbook('Минотавр СПб прайс01.02.xlsx')
 
 sheet1 = wb.sheet_by_index(0)
 sheet2 = wb.sheet_by_index(1)
@@ -12,22 +12,33 @@ sheet2 = wb.sheet_by_index(1)
 
 # for col in range(sheet1.ncols): #генерим строки
 #     print(sheet1.cell_value(5 ,col))
-
+lst_balanc = []
 for row in range(sheet1.nrows):  # генерим столбцы
-    x = str(sheet1.cell_value(row, 1)).strip()
-    y = str(sheet1.cell_value(row, 3)).strip().replace('есть', '777')
-    balance = x + ',' + y
-    if len(balance)==1:
-        
-    print(len(balance))
-    # if len(balance)<2 in balance:
-    #     balance.remove(',')
-    # print(balance)
-    # for b in balance:
-    #     b.replace('есть', '777')
-    #     print(b)
-    # # for b in balance:
-    #     b.replace('есть','777')
-    #     print(b)
-    # # balance = str(sheet1.cell_value(row, 1)) + ',' + str(sheet1.cell_value(row, 3))
+    artik = str(sheet1.cell_value(row, 1)).strip()
+    balanc = str(sheet1.cell_value(row, 3)).strip().replace('есть', '777')
+    artik_balanc = (artik + ',' + balanc).splitlines()
+    for a_b in artik_balanc:
+        if len(a_b) > 1:
+            lst_balanc.append(a_b)
+csv_balance = lst_balanc[3::]
+print(csv_balance)
+print(len(csv_balance))
+    #     if len(i) ==1:
+    #         del i
+    #
+            # balance.remove(i)
+            # print(balance)
+            # if len(balance)==1:
+
+            # print(len(balance))
+            # if len(balance)<2 in balance:
+            #     balance.remove(',')
+            # print(balance)
+            # for b in balance:
+            #     b.replace('есть', '777')
+            #     print(b)
+            # # for b in balance:
+            #     b.replace('есть','777')
+            #     print(b)
+            # # balance = str(sheet1.cell_value(row, 1)) + ',' + str(sheet1.cell_value(row, 3))
 # data = [[sheet1.cell_value(r,c) for c in range(sheet1.ncols)] for r in range(sheet1.nrows)] #Лист ексель В лист
