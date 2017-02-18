8c726a7ed664a4f5b258a593e0b6a8868ed48ad4
VitoZet <slava@gigel.ru>
2017-02-18 15:42:32 +0300

    str for float

--- main.py
+++ main.py
@@ -1,44 +1,41 @@
-import xlrd
+import xlrd, csv
 
 wb = xlrd.open_workbook('Минотавр СПб прайс01.02.xlsx')
 
 sheet1 = wb.sheet_by_index(0)
-sheet2 = wb.sheet_by_index(1)
-
-# print(sheet1.cell_value(0, 0)) #выводим первую ячейку
-# print(sheet1.nrows) #Считаем кол-во строк на листе
-# print(sheet2.nrows)
-# print(sheet1.ncols) #Считаем столбцы
-
-# for col in range(sheet1.ncols): #генерим строки
-#     print(sheet1.cell_value(5 ,col))
 lst_balanc = []
-for row in range(sheet1.nrows):  # генерим столбцы
+for row in range(sheet1.nrows):
     artik = str(sheet1.cell_value(row, 1)).strip()
     balanc = str(sheet1.cell_value(row, 3)).strip().replace('есть', '777')
-    artik_balanc = (artik + ',' + balanc).splitlines()
-    for a_b in artik_balanc:
-        if len(a_b) > 1:
-            lst_balanc.append(a_b)
-csv_balance = lst_balanc[3::]
-print(csv_balance)
-print(len(csv_balance))
-    #     if len(i) ==1:
-    #         del i
-    #
-            # balance.remove(i)
-            # print(balance)
-            # if len(balance)==1:
+    artik_balanc = (artik + ';' + balanc).splitlines()
+    price_col = str(sheet1.cell_value(row, 7)).replace('Цена клиента', '')
+    price_col2 = price_col
+    # csv_pr = (artik + ';' + price_col2)
+    print(price_col2)
+    for p in price_col2:
+        if len(p.strip()) != 0 and p.strip() != '.':
+            p = float(p)
+            # if p < 200:
+            #     p *= 1.78
+    #         elif 200<= float(p) <400:
+    #             p = p * 1,55
+    #         elif 400<= float(p) <900:
+    #             p = p * 1,47
+    #         elif 900<= float(p) <2000:
+    #             p = p * 1,43
+    #         else: p = p * 1.38
 
-            # print(len(balance))
-            # if len(balance)<2 in balance:
-            #     balance.remove(',')
-            # print(balance)
-            # for b in balance:
-            #     b.replace('есть', '777')
-            #     print(b)
-            # # for b in balance:
-            #     b.replace('есть','777')
-            #     print(b)
-            # # balance = str(sheet1.cell_value(row, 1)) + ',' + str(sheet1.cell_value(row, 3))
-# data = [[sheet1.cell_value(r,c) for c in range(sheet1.ncols)] for r in range(sheet1.nrows)] #Лист ексель В лист
+
+    # print(price_col2)
+
+
+
+#     for a_b in artik_balanc:
+#         if len(a_b) > 1:
+#             lst_balanc.append(a_b)
+# csv_balance = lst_balanc[3::]
+#
+# with open('MINO_Ballance.csv', 'w', newline='') as csv_f:
+#     csv_w = csv.writer(csv_f, delimiter = ',')
+#     for item in csv_balance:
+#         csv_w.writerow([item])
