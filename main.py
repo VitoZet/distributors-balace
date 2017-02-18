b0271519949d5a6e3ecf3749dab3603de8e5c0ac
VitoZet <slava@gigel.ru>
2017-02-18 19:29:12 +0300

    final

--- main.py
+++ main.py
@@ -3,39 +3,36 @@ import xlrd, csv
 wb = xlrd.open_workbook('Минотавр СПб прайс01.02.xlsx')
 
 sheet1 = wb.sheet_by_index(0)
-lst_balanc = []
+csv_price = []
+csv_balance = []
 for row in range(sheet1.nrows):
-    artik = str(sheet1.cell_value(row, 1)).strip()
-    balanc = str(sheet1.cell_value(row, 3)).strip().replace('есть', '777')
-    artik_balanc = (artik + ';' + balanc).splitlines()
-    price_col = str(sheet1.cell_value(row, 7)).replace('Цена клиента', '')
-    price_col2 = price_col
-    # csv_pr = (artik + ';' + price_col2)
-    print(price_col2)
-    for p in price_col2:
-        if len(p.strip()) != 0 and p.strip() != '.':
-            p = float(p)
-            # if p < 200:
-            #     p *= 1.78
-    #         elif 200<= float(p) <400:
-    #             p = p * 1,55
-    #         elif 400<= float(p) <900:
-    #             p = p * 1,47
-    #         elif 900<= float(p) <2000:
-    #             p = p * 1,43
-    #         else: p = p * 1.38
+    try:
+        row_num = float(str(sheet1.cell_value(row, 0)).strip())
+    except:
+        row_num = 0
+    if row_num > 0:
+        artik = str(sheet1.cell_value(row, 1)).strip()
+        balanc = int(float(str(sheet1.cell_value(row, 3)).strip().replace('есть', '777')))
+        price_col = float(str(sheet1.cell_value(row, 7)).replace('Цена клиента', ''))
+        if price_col < 200:
+            price_col *= 1.78
+        elif 200 <= price_col < 400:
+            price_col *= 1.55
+        elif 400 <= price_col < 900:
+            price_col *= 1.47
+        elif 900 <= price_col < 2000:
+            price_col *= 1.43
+        else: price_col *= 1.38
+        price_col_round = int(round(price_col,-1))
 
 
-    # print(price_col2)
+        csv_price.append([artik, price_col_round])
+        csv_balance.append([artik,balanc])
 
+with open('Ballance_MINO.csv', 'w', newline='') as csv_f:
+    csv_w = csv.writer(csv_f, delimiter = ',')
+    csv_w.writerows(csv_balance)
 
-
-#     for a_b in artik_balanc:
-#         if len(a_b) > 1:
-#             lst_balanc.append(a_b)
-# csv_balance = lst_balanc[3::]
-#
-# with open('MINO_Ballance.csv', 'w', newline='') as csv_f:
-#     csv_w = csv.writer(csv_f, delimiter = ',')
-#     for item in csv_balance:
-#         csv_w.writerow([item])
+with open('Price_MINO.csv', 'w', newline='') as csv_p:
+    csv_wp = csv.writer(csv_p, delimiter = ',')
+    csv_wp.writerows(csv_price)
